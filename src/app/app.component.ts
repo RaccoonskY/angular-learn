ca8401aed7ebb07a57cc03082c789ba08686f6e3
RaccoonskY <viktoryablonskas@yandex.ru>
2023-11-12 00:11:30 +0300

    added hero selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroesComponent, FormsModule],
+  imports: [CommonModule, RouterOutlet, HeroesComponent ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] 
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hero } from '../hero';
 import { HEROES } from '../mock.heroes';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, NgFor],
+  imports: [CommonModule, NgFor, NgIf, FormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
   heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
 }
