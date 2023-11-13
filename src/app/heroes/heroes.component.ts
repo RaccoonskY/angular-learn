c3a7a96a41a9cdd686ca18e9d84962ac788f9cf5
RaccoonskY <viktoryablonskas@yandex.ru>
2023-11-13 11:06:54 +0300

    added hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
 import { Hero } from '../hero';
-import { HEROES } from '../mock.heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+
+
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [CommonModule, NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService){
+  }
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
+  
+  ngOnInit(): void{
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
