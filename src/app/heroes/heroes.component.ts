4994a5cdb7c3467a5a704bc1fdb9b5eebba30d9b
RaccoonskY <viktoryablonskas@yandex.ru>
2023-11-13 11:18:04 +0300

    added message servce and component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
+
 import { Hero } from '../hero';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
-import { HeroService } from '../hero.service';
+import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 
 
@@ -14,21 +16,30 @@ import { HeroService } from '../hero.service';
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  constructor(private heroService: HeroService){
+
+  constructor(
+    private heroService: HeroService, 
+    private messageService: MessageService){
+
   }
+  
   heroes: Hero[] = [];
   selectedHero?: Hero;
+
   getHeroes(): void{
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes)
   }
-  
+
   ngOnInit(): void{
     this.getHeroes();
   }
 
+ 
+
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgIf,NgFor } from '@angular/common';
+
+import { MessageService } from '../services/message.service';
+
+@Component({
+  selector: 'app-messages',
+  standalone: true,
+  imports: [CommonModule, NgIf,NgFor],
+  templateUrl: './messages.component.html',
+  styleUrl: './messages.component.css'
+})
+export class MessagesComponent {
+  constructor(public messageService: MessageService){
+
+  }
+  
+}
